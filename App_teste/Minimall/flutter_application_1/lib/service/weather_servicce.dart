778bbfa6e0c0692c19addf9119d5150865fb0ae2
DevSNN8 <MrSouliya@gmail.com>
2024-01-04 16:01:53 +0700

    app

---
+++ App_teste/Minimall/flutter_application_1/lib/service/weather_servicce.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+
+import 'package:flutter_application_1/model/weather_model.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+
+class WeatherServicce {
+  static const BASE_URL = 'http://api.openweathermap.org/data/2.5/weather';
+  final String apikey;
+
+  WeatherServicce(this.apikey);
+
+  Future<Weather> getWeather(String cityName) async {
+    final Response = await http
+        .get(Uri.parse('$BASE_URL?q=$cityName&appid=$apikey&units=metric'));
+
+    if (Response.statusCode == 200) {
+      return Weather.fromJson(jsonDecode(Response.body));
+    } else {
+      throw Exception('Failed to load weather data');
+    }
+  }
+
+  Future<String> getCurrentCity() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+
+    List<Placemark> placemarks =
+        await placemarkFromCoordinates(position.latitude, position.longitude);
+
+    String? city = placemarks[0].locality;
+
+    return city ?? "";
+  }
+}
