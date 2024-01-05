9cf7f56ba8c33eaae7fabcb052f2afe43e89c5d3
DevSNN8 <MrSouliya@gmail.com>
2024-01-05 12:28:56 +0700

    h

---
+++ App_teste/weather_2/lib/pages/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState {}
+
+class _HomePageStage extends State<HomePage> {
+  @override
+  Widgetbuild(BuildContext context) {
+    return Scaffold();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
